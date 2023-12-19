e8eb13ba0f06664efe06ddf871a1f07148a72be6
Nicolas de Leon <nicolasmatiasdeleon@gmail.com>
2023-12-19 17:41:36 -0300

    More nits

--- src/services/thegraph.ts
+++ src/services/thegraph.ts
@@ -1,34 +1,24 @@
+import { ContractsGraphQLResponseQuery, Domains } from "./types";
 
-export type Contracts = Array<{contract: {chainId: string, address: string}}>
-export type Domains = Array<{
-  id: string
-  contracts: Contracts
-}>
-export interface ContractsGraphQLResponseQuery {
-  data: {
-    domains: Domains
-  }
-}
-
-export async function queryTheGraphGraphQl(body: string): Promise<Domains | null>   {
+export async function queryTheGraphGraphQl(body: string): Promise<Domains | null> {
   try {
     const response = await fetch('https://api.thegraph.com/subgraphs/name/alavarello/sci-goerli', {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
       },
-      body
-    })
+      body,
+    });
 
     if (response.status !== 200) {
       return null;
     }
 
-    const json: ContractsGraphQLResponseQuery = await response.json()
+    const json: ContractsGraphQLResponseQuery = await response.json();
     return json.data.domains;
 
   } catch(error: unknown) {
-    console.error(error)
-    return null
+    console.error(error);
+    return null;
   }
 }
