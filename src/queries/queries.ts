ec06ac955a1996e7bcaf9fcede3ea2dbb0e2fde9
Nicolas de Leon <nicolasmatiasdeleon@gmail.com>
2023-12-19 18:25:46 -0300

    Add missing change in query

--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -2,7 +2,7 @@ export const THE_GRAPH_CONTRACTS_WHITELISTED_QUERY = `
 query DomainWhitelistedAddresses($domain: ID!, $contract: String!) {
     domains(where: {id: $domain}) {
       id
-      contracts(where: {contract: $chainId:$address}) {
+      contracts(where: {contract: $contract}) {
         contract {
           id
           chainId
